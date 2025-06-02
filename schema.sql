fed4796bb102c8617cf897aa50c49f34d83ffe09
Flua Investimentos <contatofluainvestimentos@outlook.com>
2025-06-01 22:27:05 -0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,9 @@
 
+-- schema.sql
+
+-- Cria uma tabela simples para registrar cada vez que
+-- o usuário clica no botão START.
+CREATE TABLE IF NOT EXISTS processos (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+);
